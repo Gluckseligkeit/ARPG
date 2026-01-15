b859ee6ff39f1e663b1453242b08860427c4037b
Gluckseligkeit <nan.thxd@gmail.com>
2026-01-15 15:01:17 -0300

    Day6

--- Source/ARPG/Public/Player/MainPlayerController.h
+++ Source/ARPG/Public/Player/MainPlayerController.h
@@ -6,6 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "MainPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -13,5 +17,19 @@ UCLASS()
 class ARPG_API AMainPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AMainPlayerController();
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> PlayerContext;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	void Move(const FInputActionValue& InputActionValue);
 };
