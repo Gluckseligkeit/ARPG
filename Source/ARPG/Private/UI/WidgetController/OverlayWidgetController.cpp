24a0d7de3086efd338f801e115921f7994d43c4c
Gluckseligkeit <nan.thxd@gmail.com>
2026-01-18 13:08:59 -0300

    Day58

--- Source/ARPG/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/ARPG/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -35,12 +35,14 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		ARPGAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 	
 	Cast<UARPGAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[](const FGameplayTagContainer& AssetTags)
+		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
 				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, Msg);
+				
+				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
 			}
 		}
 	);
