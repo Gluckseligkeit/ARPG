c23e2e954e34d6489cefee5cb2c20708368e0564
Gluckseligkeit <nan.thxd@gmail.com>
2026-01-18 11:14:23 -0300

    Day56

--- Source/ARPG/Private/AbilitySystem/ARPGAbilitySystemComponent.cpp
+++ Source/ARPG/Private/AbilitySystem/ARPGAbilitySystemComponent.cpp
@@ -10,6 +10,13 @@ void UARPGAbilitySystemComponent::AbilityActorInfoSet()
 
 void UARPGAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
 	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
-{
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Applied"));
+{	
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO: Broadcast tag to widget controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, Msg);
+	}
 }
