df25ae4d79ffe3989dc0da1a3bf047193b08836e
Gluckseligkeit <nan.thxd@gmail.com>
2026-02-16 04:37:33 -0300

    startover

--- Source/ARPG/Private/Actor/ARPGProjectile.cpp
+++ Source/ARPG/Private/Actor/ARPGProjectile.cpp
@@ -55,9 +55,7 @@ void AARPGProjectile::Destroyed()
 
 void AARPGProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                       UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Overlap with: %s"), *OtherActor->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("Has Authority: %s"), HasAuthority() ? TEXT("True") : TEXT("False"));
+	{
 	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
@@ -86,7 +84,7 @@ void AARPGProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 	{
 		bHit = true;
 	}
-}
+};
 
 
 
